20dfb38e7f6610f0b9da0172c1037a30b9428396
Nicolas <pnschlei@gmail.com>
2020-11-19 21:10:48 +0100

    Fix Maze.txt and create a Grid method

--- main.py
+++ main.py
@@ -1,44 +1,41 @@
 # coding: utf-8
 
-
-"""Pour générer notre labyrinthe nous allons utilisé 
-un algorithme de parcours en profondeur"""
-
-
-
-class Square:
-	"""Mon labyrinthe est un grille composé
-		de plusieurs petits carrés entourés par des murs"""	
-
-	
-	def __init__(self): 
-		"""on initialise notre classe carré.Avec nos 4 cotés."""
-		self.top = True
-		self.bottom = True
-		self.left = True
-		self.right = True
-	
-
 class Maze:
-	"""Ma grille aura 15 carrés de longueur et 15 carrés de largeur"""
-	def __init__(self):
-		"""On creer la grille.Notre grille est en fait une liste
-		qui contient 15 autres listes de carrés """
-		self.a_list = []
-		self.append(Square()) 
-		
+    """Ma grille aura 15 carrés de longueur et 15 carrés de largeur"""
+    def __init__(self):
+        """le labyrinthe est une grille."""
+        self.grid = {}
+
+    def read_maze(self):
+        with open("Maze.txt","r") as maze_file: #ouvre le fichier
+            contents = maze_file.read() #lire le fichier Maze.txt
+            contents = contents.split()
+            grid = {}
+            for key,value in enumerate(contents):
+                grid[key] = value
+            for key in range(15):
+                print(value * 3)
+        
+
+            
+
+
+class MacGyver:
+    """ personnage MacGyver"""
+    pass
+
+class Guardian:
+    """ le gardien """
+    pass
+
 
 
 def main():
- """test pour creer une liste de liste"""
-	liste_liste = []
-	liste = []
-	for x in range(15):
-		liste.append(Square())
-
-	for i in range(15):
-		liste_liste.append(liste)
-	print(liste_liste[4][5].top)
+    """test pour creer une liste de liste"""
+    maze = Maze()
+    print(maze.read_maze())
+    
+    
 main() 
 
 
