7bd87123d6534cf3d7fd1b63df1ffd4ddcb23b59
Nicolas <pnschlei@gmail.com>
2020-11-24 19:52:34 +0100

    print way method

--- main.py
+++ main.py
@@ -1,51 +1,57 @@
 # coding: utf-8
 
 class Maze:
-    """Ma grille aura 15 carrés de longueur et 15 carrés de largeur"""
-    def __init__(self):
-        """le labyrinthe est une grille."""
-        self.grid = {}
+	"""Ma grille aura 15 carrés de longueur et 15 carrés de largeur"""
+	def __init__(self):
+		"""le labyrinthe est une grille."""
+		self.grid = {}
 
-    def read_maze(self):
-        with open("Maze.txt","r") as maze_file: #ouvre le fichier
-            line = maze_file.readline() #lire le fichier ligne par ligne Maze.txt
-            grid = {}
-            lines = []
-            for element in line:
-                element = line.split()
-                lines.append(element)
-                print(element)
-                line = maze_file.readline() #ligne suivante
-            for i, row in enumerate(lines):
-                for x, y in enumerate(row):
-                    grid[i, x] = y
-            print(grid)
-            
-            # grid([x, y]) = "chemin"
-            # grid([x, y]) = "McGyver"
-            # grid([x, y]) = "Gardien"
-            
-            
-           
-            
-            
-            
-
+	def read_maze(self):
+		with open("Maze.txt","r") as maze_file: #ouvre le fichier
+			line = maze_file.readline() #lire le fichier ligne par ligne Maze.txt
+			grid = {}
+			lines = []
+			while line != "":
+				element = line.split()
+				lines.append(element)
+				print(element)
+				line = maze_file.readline()    
+			for i, row in enumerate(lines):
+				for x, y in enumerate(row):
+					grid[i, x] = y
+			for key, value in grid.items():
+					#print(key,value)
+					pass      
+			# grid([x, y]) = "chemin"
+			# grid([x, y]) = "McGyver"
+			# grid([x, y]) = "Gardien"
+			
+			
+	def waylist(self):
+		with open("Maze.txt","r") as maze_file:
+			way = maze_file.readline()
+			while way != "":
+				true_way = way.replace("#", ".")
+				print(true_way)
+				way = maze_file.readline()
+				
+			
 class MacGyver:
-    """ personnage MacGyver"""
-    pass
+	""" personnage MacGyver"""
+	pass
 class Guardian:
-    """ le gardien """
-    pass
+	""" le gardien """
+	pass
 
 
 
 def main():
-    """Fonction principale"""
-    maze = Maze()
-    print(maze.read_maze())
-    
-    
+	"""Fonction principale"""
+	maze = Maze()
+	print(maze.waylist())
+
+	
+	
 main() 
 
 
