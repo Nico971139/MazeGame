bd1e00cb28dd592502fe3c288818e5ae228fdd81
Nicolas <pnschlei@gmail.com>
2020-12-03 20:51:42 +0100

    update project

--- main.py
+++ main.py
@@ -1,98 +1,45 @@
 # coding: utf-8
 import random
 
-
 class Maze:
 	"""Une grille de 15 cellules par 15 cellule"""
 	def __init__(self):
 		"""le labyrinthe est une grille."""
-		self.maze_file = open("Maze.txt", "r")
 		self.grid = {}
-		self.way_list = {}
-
-
+		self.way_list = []
+		self.read_maze()
+		self.set_objects()
+		
 
 	def read_maze(self):
-		
-		line = self.maze_file.readline() #lire le fichier ligne par ligne Maze.txt
-		
-		lines = []
-		while line != "":
-			element = line.split()
-			lines.append(element)
-			#print(element)
-			line = self.maze_file.readline()
+		x, y = 0, 0
+		with open("Maze.txt", "r") as line:
+			l = line.read()
+			for element in l:
+				if element == '\n':
+					x, y = 0, y+1
+				else :
+					self.grid[(x, y)] = element
+					if element == "0":
+						self.way_list.append((x, y))
+					x += 1
 
-		for i, row in enumerate(lines):
-			for x, y in enumerate(row):
-				self.grid[i, x] = y
+	def set_objects(self):
+		objects = ["T", "S", "E"]
+		random.shuffle(self.way_list)
+		for key, value in enumerate(objects):
+			self.grid[self.way_list[key]] = value
 
-	
-	def waylist(self):
-		"""Recuperer la liste de chemin"""
 
-		for key,value in self.grid.items():
-			if value == "0":
-				self.way_list[key] = value
-			else:
-				pass
-		
-		"""for k in list(way):
-			for n in range(3):	
-				random.choice(list(way))"""
-		
 class MacGyver:
-	""" personnage MacGyver"""
+	"""Personnage principal"""
 	def __init__(self):
 		pass
 
-	def movement(self):
-		pass
 
-	def takeItems(self):
-		pass
-
-
-class Guardian:
-	""" le gardien """
-	pass
-
-class Items(Maze):
-	""" les objets """
-	def __init__(self ):
-		super().__init__()
-		self.characters = "Z"
-		self.position = [random.choice(self.way_list.key()) for k in self.way_list]
-			
-	"""def put_items(self):
-		for k in list(self.way_list):
-			for n in range(3):
-				print(random.choice(list(self.way_list)))"""
-			
-				
-
-	
-
-def main():
+if __name__ == '__main__':
 	"""Fonction principale"""
 	maze = Maze()
-	macGyver = MacGyver()
-	maze.read_maze()
-	maze.waylist()
-	items = Items() #Initialiser les objets
-	items.read_maze()
-	items.waylist()
-	#Positioner les objet sur les chemins
-	print(items.way_list)
-	
-	
-
-
-
-	
-main() 
-
-
-
+	print(maze.grid)
 
 
