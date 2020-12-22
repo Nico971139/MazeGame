d9aac1cbefecc68dbb4e4bde74c4cde6e7b03e27
Nicolas <pnschlei@gmail.com>
2020-12-22 01:35:22 +0100

    fix movement

--- maze.py
+++ maze.py
@@ -2,29 +2,30 @@
 import random
 import pygame
 
-class Maze():
-	"""Une grille de 15 cellules par 15 cellule"""
+class Maze(pygame.sprite.Sprite):
+	"""Grid with coordinate keys and values
+	Player start at "D" if he pick up all items
+	he win, else he lose the game"""
 	def __init__(self):
-		"""le labyrinthe est une grille."""
+		super().__init__()
+		"""A dict for coordinates
+		   a list for the way. And a method to set position
+		   of the objects"""
 		self.grid = {}
 		self.way_list = []
 		self.macGyver = None
 		self.guardian = None
 		self.inventory = []
+		self.image = pygame.image.load('ressource/Mario.png')
 		self.read_maze()
 		self.set_objects()
 		
 
 	def read_maze(self):
-		"""On va lire le fichier maze.txt et 
-		pour chaque element du fichier on lui
-		attribut une abscisse et une ordonnée"""
 		x, y = 0, 0
 		with open("Maze.txt", "r") as line:
 			l = line.read()
-			# pour chaque element du fichier
 			for element in l:
-				# si on va à la ligne
 				if element == '\n':
 					x, y = 0, y+1
 				else :
@@ -38,8 +39,6 @@ class Maze():
 					x += 1
 
 	def set_objects(self):
-		"""crée une liste d'objets qu'on 
-		place aleatoirement sur le chemin"""
 		objects = ["T", "S", "E"]
 		random.shuffle(self.way_list)
 		for key, value in enumerate(objects):
@@ -50,22 +49,20 @@ class Maze():
 	########################             
 
 	def check_move(self, a, b):
-		"""vérifier que la destination n'est pas un mur"""
-		"""vérifier que la destination n'est pas en dehors de la grille du labyrinthe"""
+		"""check the destination"""
 		return ((a, b) in self.grid and self.grid[a, b] != "#")
 
 	def check_items(self, a, b):
-		"""	vérifier s'il y a un objet ou non sur la case de destination
-			"ramasser" cet objet. redéfinir la valeur de la clé dans la grille à "chemin"
-			ajouter l'objet ramassé dans le sac à dos de MacGyver"""
+		"""	check if an object is on the way
+			pick up this object. 
+			add object in MacGyver inventory"""
 		if self.grid[a, b] == "T" or self.grid[a, b] == "S" or self.grid[a, b] ==  "E":
 			self.inventory.append(self.grid[a, b])
 			print(self.grid[a, b])
 			self.grid[a, b] = "0"
 
 	def check_pos_guardian(self, a, b):
-		"""vérifier s'il y a le gardien
-		ou non sur la case de destination"""
+		"""check the guardian position"""
 		if self.grid[a, b] == self.grid[self.guardian]:
 			if len(self.inventory) == 3:
 				print("You win")
@@ -77,11 +74,13 @@ class Maze():
 	########################
 	def top(self):
 		x = self.macGyver[0]
-		y = self.macGyver[1] - 1
+		y = self.macGyver[1] - 1	
 		if self.check_move(x, y):
 			self.macGyver = x, y
 			self.check_items(x, y)
 			self.check_pos_guardian(x, y)
+			# update rect position
+			self.rect.x , self.rect.y = x*20, y*20
 
 	def bot(self):
 		x = self.macGyver[0]
@@ -90,7 +89,8 @@ class Maze():
 			self.macGyver = x, y
 			self.check_items(x, y)
 			self.check_pos_guardian(x, y)
-
+			# update rect position
+			self.rect.x , self.rect.y = x*20, y*20
 
 	def left(self):
 		x = self.macGyver[0] - 1
@@ -99,6 +99,8 @@ class Maze():
 			self.macGyver = x, y
 			self.check_items(x, y)
 			self.check_pos_guardian(x, y)
+			# update rect position
+			self.rect.x , self.rect.y = x*20, y*20
 
 	def right(self):
 		x = self.macGyver[0] + 1 
@@ -107,35 +109,47 @@ class Maze():
 			self.macGyver = x, y
 			self.check_items(x, y)
 			self.check_pos_guardian(x, y)
-	
-class MacGyver():
-	"""Personnage principal"""
+			# update rect position
+			self.rect.x , self.rect.y = x*20, y*20
+
+class MacGyver(Maze):
+	"""Hero"""
 	def __init__(self):
-		self.inventory = []
-		self.image = pygame.image.load('ressource/Mario.png').convert_alpha()
+		super().__init__()
+		"""Init the object player. Player is a rectangle
+		with an image. It's set with attribut self.macGyver * the block size"""
+		self.rect = self.image.get_rect()
+		self.rect.x = self.macGyver[0] * 20 
+		self.rect.y = self.macGyver[1] * 20
 
 
 class Game():
 	""" Game display """
 	def __init__(self):
-		self.size = self.width, self.height = 640, 480
+		"""Attributes of the game screen """
+		self.size = self.width, self.height = 300, 300
 		self.screen = pygame.display.set_mode(self.size)
 		self.wall = pygame.image.load('ressource/mur.png')
 		self.way = pygame.image.load('ressource/chemin.png')
 		self.boss_image = pygame.image.load('ressource/gardien.png')
-
-
+		self.syringe = pygame.image.load('ressource/seringue.png')
+		self.ether = pygame.image.load('ressource/ether.png')
+		self.tube = pygame.image.load('ressource/tube.png')
 	
 
 
 if __name__ == '__main__':
-	"""Fonction principale"""
+	"""test function"""
+	maze = Maze()
 	game = Game()
 	player = MacGyver()
-	print(game.size)
-	print(game.width)
-	print(game.wall)
-
+	player.bot()
+	player.bot()
+	player.bot()
+	print(player.macGyver)
+	print(player.rect)
+	print(player.rect.x)
+	print(player.rect.y)
 
 	
 	
--- screen.py
+++ screen.py
@@ -1,14 +1,17 @@
 # coding: utf-8
-from maze import *
+from maze import Maze, Game, MacGyver
 import pygame, sys
 pygame.init()
 
-maze = Maze()
 game = Game()
 player = MacGyver()
 wall = game.wall
 way = game.way
+syringe = game.syringe
+ether = game.ether
+tube = game.tube
 player_image = player.image
+player_rect = player_image.get_rect()
 boss = game.boss_image
 # Create the display surface
 screen = game.screen
@@ -16,23 +19,48 @@ pygame.display.set_caption("MAZE GAME")
 
 
 x, y = 0, 0
+""" x = absciss on screen 
+	y = ordinate on screen
+	20 = size of block """
 for y in range(15):
 	for x in range(15):
-		if maze.grid[(x, y)] == "#":
+		if player.grid[(x, y)] == "#":
 			screen.blit(wall, (x*20, y*20))
-		if maze.grid[(x,y)] == "0":
+		if player.grid[(x,y)] == "0":
 			screen.blit(way, (x*20, y*20))
-		if maze.grid[(x,y)] == "D":
-			screen.blit(player_image, (x*20, y*20))
-		if maze.grid[(x,y)] == "F":
+		if player.grid[(x,y)] == "D":
+			screen.blit(way, (x*20, y*20))
+			screen.blit(player_image, player.rect)
+		if player.grid[(x,y)] == "F":
 			screen.blit(boss, (x*20, y*20))
-
+		if player.grid[(x, y)] == "T":
+			screen.blit(tube, (x*20,y*20))
+		if player.grid[(x, y)] == "S":
+			screen.blit(syringe, (x*20,y*20))
+		if player.grid[(x, y)] == "E":
+			screen.blit(ether, (x*20,y*20))
 # game loop
 while 1:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT: sys.exit()
 
-		
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_RIGHT:
+				player.right()
+				screen.blit(player_image, player.rect)
+				pygame.display.flip()
+			elif event.key == pygame.K_LEFT:
+				player.left()
+				screen.blit(player_image, player.rect)
+				pygame.display.flip()
+			elif event.key == pygame.K_UP:
+				player.top()
+				screen.blit(player_image, player.rect)
+				pygame.display.flip() 
+			elif event.key == pygame.K_DOWN:
+				player.bot()
+				screen.blit(player_image, player.rect)
+				pygame.display.flip()
 
 
 
